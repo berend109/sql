406b595a745e6b24b7dadb9a7b1b719ca99a2ad3
berend109 <berend109@gmail.com>
2022-09-07 15:54:15 +0200

    Start Week 2

---
+++ Week2/Week2.sql
@@ -0,0 +1,45 @@
+-- 1.	In welke staat is president R(onald) Reagan geboren en in welk jaar? Let op de schrijfwijze van de naam!
+SELECT State_born, Birth_Yr
+FROM Pres
+WHERE Pres_name = "Reagan R"
+-- 2.	Geef alle geregistreerde gegevens over de huwelijken van president Reagan.
+
+-- 3.	In welk(e) ja(a)r(en) werd Reagan president?
+
+-- 4.	Welke staten hebben de Union (de United States of America) opgericht?
+
+-- 5.	Geef een lijst van alle door presidenten beoefende en geregistreerde hobby's. Zorg dat deze lijst aflopend gesorteerd is.
+
+-- 6.	Welke personen zijn ooit vice-president geweest?
+
+-- 7.	Welke presidentshuwelijken zijn kinderloos gebleven? Toon ook het aantal kinderen.
+
+-- 8.	Welke presidenten hielden van paardrijden (Riding)? Toon ook de hobby!
+
+-- 9.	Welke presidenten houden van lezen (reading) of vissen (fishing)?
+
+-- 10.	In welk jaar kwam Hawaii bij de United States of America?
+
+-- 11.	Welke presidenten zijn ouder dan 85 geworden? Geef de naam, leeftijd en geboortejaar van de president.
+
+-- 12.	Van welke presidenten is de geboortestaat niet bekend? Toon naam en geboortestaat. 
+
+-- 13.	Hoeveel kinderen heeft president Tyler uit  zijn 1e huwelijk?
+
+-- 14.	Geef de verkiezingsgegevens, geordend op verkiezingsjaar, van de winnaars (wl_indic) van de presidentsverkiezingen die in vorige eeuw (de 20e) zijn gehouden.
+
+-- 15.	Geef de naam,leeftijd, partij en het aantal dienstjaren van die presidenten die reeds zijn overleden, niet republikeins waren en minstens 70 jaar oud zijn geworden. 
+
+-- 16.	In welke regeringen heeft een Roosevelt zitting gehad (als president of als vice-president)? 
+
+-- 17.	Welke kandidaten bij presidentsverkiezingen hebben 0 stemmen gehaald ? 
+
+-- 18.	Welke verliezers van de presidentsverkiezingen hebben tussen 150 en 250 stemmen behaald ?  Toon naam en aantal stemmen en WL-indic (winn-loss-indicator)
+
+-- 19.	Welke presidenten zijn geboren in een van de zuidelijke staten (Georgia, South Carolina, Alabama, Mississippi, Louisiana, Texas, Arkansas, North Carolina, Tennessee, Virginia of Florida)? Toon presidentnaam en staatnaam en sorteer op presidentnaam.
+
+-- 20.	Welke presidenten zijn niet van de republikeinse partij? Toon presidentnaam en partij en sorteer op partijnaam aflopend en dan op presidentnaam oplopend.
+
+-- 21.	Zijn er huwelijken met 5 of meer kinderen of maar met 1 kind? Laat zien welk huwelijk en hoeveel kinderen. Sorteer Oplopend.
+
+-- 22.	Zijn er presidenten die gestorven zijn voor hun zestigste levensjaar?
